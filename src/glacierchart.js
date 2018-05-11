c2acc4b99880987d6210f7f1c5f05abfed5110c4
Andreas-Keller <andreaskeller2010@gmail.com>
2018-05-11 11:22:01 +0200

    new data

--- src/glacierchart.js
+++ src/glacierchart.js
@@ -7,139 +7,135 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1IjoiYW5kcmVhcy1rZWxsZXIiLCJhIjoiY2pneHZrY3ZnMW0yczMycGQzMGp1YXl5aSJ9.BJhSa5Htz16Rm-st7l5QaQ'
 }).addTo(mymap);
 
-var marker = L.marker([46.8, 8.9]).addTo(mymap)
-// marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+var popups = {}
 
-var popup = L.popup({minWidth: 400})
+function newPopup(filename){
+    var glacierdata;
+    d3.csv(filename, function(error, data) {
 
-// create svg canvas
-const canvHeight = 300, canvWidth = 400;
-var container = document.createElement("div")
-const svg = d3.select(container).append("svg")
-    .attr("width", canvWidth)
-    .attr("height", canvHeight)
-    .style("border", "1px solid");
+        glacierdata = data
+        var latitude = glacierdata[6]['col2']
+        var longitude = glacierdata[6]['col3']
+        latitude *= 1000
+        longitude *= 1000
 
-// calc the width and height depending on margins.
-const margin = {top: 50, right: 80, bottom: 50, left: 60};
-const width = canvWidth - margin.left - margin.right;
-const height = canvHeight - margin.top - margin.bottom;
+        var coordsGPS = Swisstopo.CHtoWGS(latitude, longitude);
+        var marker = L.marker([coordsGPS[1], coordsGPS[0]]).addTo(mymap)
 
-// create parent group and add left and top margin
-const g = svg.append("g")
-    .attr("id", "chart-area")
-    .attr("transform", "translate(" +margin.left + "," + margin.top + ")");
+    // create svg canvas
+        const canvHeight = 300, canvWidth = 400;
+        var container = document.createElement("div")
+        const svg = d3.select(container).append("svg")
+            .attr("width", canvWidth)
+            .attr("height", canvHeight)
+            //.style("border", "1px solid");
 
+    // calc the width and height depending on margins.
+        const margin = {top: 20, right: 20, bottom: 30, left: 30};
+        const width = canvWidth - margin.left - margin.right;
+        const height = canvHeight - margin.top - margin.bottom;
 
-d3.csv('./data/aletsch.csv', function(error, data) {
-    console.log(data)
-    g.append("text").text(data[0]['col2'])
-})
+        svg.append("text")
+            .attr("y", 0)
+            .attr("x", canvWidth / 2)
+            .attr("dy", "1em")
+            .attr("font-family", "sans-serif")
+            .attr("font-size", "12px")
+            .style("text-anchor", "middle")
+            .text(glacierdata[0]['col2'])
 
-function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent(container)
-        .openOn(mymap);
-}
+    // create parent group and add left and top margin
+        const g = svg.append("g")
+            .attr("id", "chart-area")
+            .attr("transform", "translate(" +margin.left + "," + margin.top + ")");
 
-marker.on('click', onMapClick);
+        const dataOnly = glacierdata.slice(11, glacierdata.length)
 
+        const dataValues = d3.values(dataOnly)
 
-/*
-// create svg canvas
-const canvHeight = 600, canvWidth = 800;
-const svg = d3.select("body").append("svg")
-    .attr("width", canvWidth)
-    .attr("height", canvHeight)
-    .style("border", "1px solid");
+        var lengthChangeDomain = d3.extent(dataValues, d => Number(d.col3)).reverse();
 
-// calc the width and height depending on margins.
-const margin = {top: 50, right: 80, bottom: 50, left: 60};
-const width = canvWidth - margin.left - margin.right;
-const height = canvHeight - margin.top - margin.bottom;
+        if (lengthChangeDomain[0] < 0){
+            lengthChangeDomain[0] = 0;
+        }
 
-// create parent group and add left and top margin
-const g = svg.append("g")
-    .attr("id", "chart-area")
-    .attr("transform", "translate(" +margin.left + "," + margin.top + ")");
+        const yearDomain = d3.extent(dataValues, d => Number(d.col2))
 
+        const xScale = d3.scaleLinear()
+            .rangeRound([0,width])
+            .domain(yearDomain)
+            .nice(5);
 
-d3.csv('./data/who-life-expectancy-by-country.csv', function(error, data) {
-    
-    const dataNested = d3.nest()
-        .key(function(d) { return d.Year; }).sortKeys(d3.ascending)
-        //.key(function(d) { return d.Country; }).sortKeys(d3.ascending)
-        .entries(data);
-
-    //console.log(dataNested[0]);
-
-    const dataN = Object.values(dataNested[0])[1];
-
-   // console.log(dataN);
-
-    const lifeExpectancyDomain = d3.extent(dataN, d => Number(d.LifeExpectancyAtBirth));
-
-    //const countriesDomain = d3.set(dataN, d => d.Country);
-
-    const countriesDomain = [185,0];
-
-    // create scales for x and y direction
-    const xScale = d3.scaleLinear()
-        .rangeRound([0,width])
-        .domain(lifeExpectancyDomain)
-        .nice(5);
-
-    const yScale = d3.scaleLinear()
-        .rangeRound([0,height])
-        .domain(countriesDomain);
+        const yScale = d3.scaleLinear()
+            .rangeRound([height,0])
+            .domain(lengthChangeDomain);
         //.nice(5);
 
-    const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+        // create xAxis
+        const xAxis = d3.axisBottom(xScale)
+        xAxis.tickFormat(d3.format(".4"))
 
-    // create xAxis
-    const xAxis = d3.axisBottom(xScale);
-    g.append("g")  // create a group and add axis
-        .attr("transform", "translate(0," + height + ")").call(xAxis);
+        g.append("g")  // create a group and add axis
+            .attr("transform", "translate(0," + height + ")").call(xAxis);
 
-    // create yAxis
-    const yAxis = d3.axisLeft(yScale);
-    g.append("g")  // create a group and add axis
-        .call(yAxis);
+        // create yAxis
+        const yAxis = d3.axisLeft(yScale);
+        g.append("g")  // create a group and add axis
+            .call(yAxis);
 
-    // add circle
-    var data_points = g.selectAll("rect")  // this is just an empty placeholder
-        .data(dataN)
-        .enter()
-        .append("rect")
-        .attr("x", 5)
-        .attr("y", (d,i)=> yScale(i)-3)
-        .attr("width", function(d) { return xScale(d.LifeExpectancyAtBirth) +"" })
-        .attr("height", "1")
-        .attr("stroke", "green")
-        .text(function(d){return d.Country+"" })
+        var data_points = g.selectAll("rect")
+            .data(dataValues)
+            .enter()
+            .append("rect")
+            .attr("x", function(d){ return xScale(d.col2)})
+            .attr("y", function(d) { return d.col3 < 0 ? yScale(d.col3) : yScale(0) })
+            .attr("height", function(d) { return d.col3 < 0 ? yScale(0) - yScale(d.col3) : yScale(d.col3) - yScale(0) })
+            .attr("width", "1")
+            .attr("stroke", "red")
+            //.text(function(d){return d.col3+""  });
 
-    var tooltip = d3.select("body").append("div").classed("tooltip", true);
+        var tooltip = d3.select(container).append("div").classed("tooltip", true);
 
-    data_points.on("mouseover", function(d, i) {
-        tooltip
-            .html(`Country: ${d.Country}<br/>`
-                + `LifeExpectancy: ${d.LifeExpectancyAtBirth}<br/>`)
-            .style("visibility", "visible")
-            .style("left", (d3.event.pageX) + "px")
-            .style("top", (d3.event.pageY - 28) + "px");
+        data_points.on("mouseover", function(d, i) {
+            tooltip
+                .html(`${d.col3}m`)
+                .style("visibility", "visible")
+                .style("left", width + 20 + "px")  //d3.event.pageX
+                .style("top",  margin.top + "px")  //d3.event.pageY
+                .style("position", "absolute");
+        })
+            .on("mouseout", function(d,i) {
+                tooltip.style("visibility", "hidden")
+            });
+
+        popups[filename] = L.popup({minWidth: 400})
+        popups[filename].setLatLng([coordsGPS[1], coordsGPS[0]])
+        popups[filename].setContent(container)
+        marker.on('click', onMapClick, popups[filename]);
     })
-        .on("mouseout", function(d,i) {
-            tooltip.style("visibility", "hidden")
-        });
-});
+}
 
-svg.append("text")
-    .attr("y", 10)
-    .attr("x", canvWidth / 2)
-    .attr("dy", "1em")
-    .attr("font-family", "sans-serif")
-    .attr("font-size", "26px")
-    .style("text-anchor", "middle")
-    .text("Life Expectancy in 2002 worldwide");
-*/
+function onMapClick(){
+   this.openOn(mymap);
+}
+
+newPopup('./data/aletsch.csv')
+newPopup('./data/allalin.csv')
+newPopup('./data/alpetli.csv')
+newPopup('./data/ammerten.csv')
+newPopup('./data/arolla.csv')
+newPopup('./data/bellatola.csv')
+newPopup('./data/biferten.csv')
+newPopup('./data/bis.csv')
+newPopup('./data/bluemlisalp.csv')
+newPopup('./data/brunegg.csv')
+newPopup('./data/brunni.csv')
+newPopup('./data/damma.csv')
+newPopup('./data/kaltwasser.csv')
+newPopup('./data/kehlen.csv')
+newPopup('./data/kessjen.csv')
+newPopup('./data/orny.csv')
+newPopup('./data/scaletta.csv')
+newPopup('./data/morteratsch.csv')
+newPopup('./data/verstankla.csv')
+newPopup('./data/zmutt.csv')
